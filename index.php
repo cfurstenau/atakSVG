33cb56ee312979389a66a53ba35b07803878186f
medievalhellspawn999 <medievalhellspawn999@users.noreply.github.com>
2017-03-25 10:36:06 -0700

    Add files via upload
    A few changes to the index.php

--- index.php
+++ index.php
@@ -1,4 +1,3 @@
-
 <html>
   <head>
     <title>AT:AK SVG</title>
@@ -8,7 +7,7 @@
 	<link href="css/style.css" type="text/css" rel="stylesheet" />
 	
 	<!--Function to change out the colors and text content value pair-->
-    <script src='js/colorMod.js'></script>
+        <script src='js/colorMod.js'></script>
 	<script src='js/valueMod.js'></script>
 
   </head>
@@ -33,45 +32,46 @@
 	  
 	  /* Get status from tag */
 	  $tag= $_REQUEST['tag'];
-	  $sqltag = 'select statusID from tagStatus where tag="'.$tag.'"';
-	  $resulttag = mysql_query($sqltag);
-	  $num_results = mysql_num_rows($resulttag); 
+	  $sqltag = 'select statusID from tag_status where tag="'.$tag.'"';
+	  $resulttag = mysqli_query($conn,$sqltag);
+	  $num_results = mysqli_num_rows($resulttag); 
 	  
-	  /* Get colors from status IDs */
-	  while ($rowtag = mysql_fetch_array($resulttag)) {
-	    $sql = "select name,color from status where id in (".$rowtag['statusID'].")";
-	    $result = mysql_query($sql);
-	  }
+
 	  
 	  /* Get value pair from tag  for text content replacement*/
-	  $sqltag2 = 'select valueID from tagValue where tag="'.$tag.'"';
-	  $resulttag2 = mysql_query($sqltag2);
-	  $num_results2 = mysql_num_rows($resulttag2); 
+	  $sqltag2 = 'select valueID from tag_value where tag="'.$tag.'"';
+	  $resulttag2 = mysqli_query($conn,$sqltag2);
+	  $num_results2 = mysqli_num_rows($resulttag2); 
 	  	 
 	  
 	  /* Call colorMod and valueMod function call through a list of elements by name with specified tag with some result */
 	  echo "<script>";
 	  
+	  /* Get colors from status IDs */
 	  if ( $num_results > 0 ) {
-	    while ($row = mysql_fetch_array($result)) {
+	    while ($rowtag = mysqli_fetch_array($resulttag)) {
+	      $sql = "select name,color from status where id in (".$rowtag['statusID'].")";
+	      $result = mysqli_query($conn,$sql);
+	    }
+	    while ($row = mysqli_fetch_array($result)) {
 	      echo "colorMod('".$row['name']."','".$row['color']."');";
 	    }
 	  }
 	  
 	  /* Get value from value IDs */
 	  if ( $num_results2 > 0 ) {
-	    while ($rowtag2 = mysql_fetch_array($resulttag2)) {
+	    while ($rowtag2 = mysqli_fetch_array($resulttag2)) {
 	      $sql2 = "select name,value from value where id in (".$rowtag2['valueID'].")";
-	      $result2 = mysql_query($sql2);
+	      $result2 = mysqli_query($conn,$sql2);
 	    }
-	    while ($row2 = mysql_fetch_array($result2)) {
+	    while ($row2 = mysqli_fetch_array($result2)) {
 	      echo "valueMod('".$row2['name']."','".$row2['value']."');";
 	    }
 	  }
 	     
 	  echo "</script>";
 	  
-	  include('dbclose.php');
+	  mysqli_close($conn);
 	
 	}
 	/* -------------------------------------- */
