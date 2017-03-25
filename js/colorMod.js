7a1c1ee4cc5cfc96303103af0b578511cb0d3724
medievalhellspawn999 <medievalhellspawn999@users.noreply.github.com>
2017-03-25 10:30:39 -0700

    Add files via upload

--- js/colorMod.js
+++ js/colorMod.js
@@ -6,6 +6,16 @@ function colorMod(name,color){
 		  element.style.stroke="black";
 		  element.style.strokeWidth="1";
 		  element.style.strokeDasharray="3,1";
+		  
+		  var ani = document.createElementNS("http://www.w3.org/2000/svg","animate");
+		  ani.setAttribute("attributeName","opacity");
+		  ani.setAttribute("repeatCount","indefinite");
+		  ani.setAttribute("from","0");
+		  ani.setAttribute("to","1");
+		  ani.setAttribute("dur","2s");
+		
+		  element.appendChild(ani);
+
 		}
 	  }
 } 
